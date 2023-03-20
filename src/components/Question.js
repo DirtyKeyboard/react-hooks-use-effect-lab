c836850c2b449690fc2fd480415fc7c2b9ab992f
DirtyKeyboard <andrewxx000222@icloud.com>
2023-03-20 16:15:23 -0500

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0)
+    {
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    else
+    {
+      const timer = setTimeout(() => {
+        setTimeRemaining((old) => old-1)
+      },1000)
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+  })
 
+  // add useEffect code
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
